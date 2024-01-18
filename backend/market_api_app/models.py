33b162aa6f7ff443561176483e741943d3d4d870
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2024-01-19 01:00:01 +0200

    feat: redefine AppUser entity

--- backend/market_api_app/models.py
+++ backend/market_api_app/models.py
@@ -1,7 +1,91 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 order_type_choices = [('BUY', 'Buy'), ('SELL', 'Sell')]
 order_status_choices = [('ACTIVE', 'Active'), ('EXECUTED', 'Executed')]
+
+
+class AppUserManager(BaseUserManager):
+    @staticmethod
+    def validate_birthdate(date):
+        # TODO: IMPLEMENT DATE VALIDATION
+        return date
+
+    @staticmethod
+    def normalize_phone_number(phone_number):
+        # TODO: IMPLEMENT PHONE_NUMBER VALIDATION
+        return phone_number
+
+    def create_user(self, first_name, second_name, phone_number, birthday, email, password=None):
+        if not first_name:
+            raise ValueError('A first name is required!')
+        if not second_name:
+            raise ValueError('A second name is required!')
+        if not email:
+            raise ValueError('An email is required!')
+        if not password:
+            raise ValueError('An password is required!')
+
+        email = self.normalize_email(email)
+        norm_phone_number = self.normalize_phone_number(phone_number)
+        norm_birthdate = self.validate_birthdate(birthday)
+
+        user = self.model(email=email,
+                          first_name=first_name,
+                          second_name=second_name,
+                          phone_number=norm_phone_number,
+                          birthday=norm_birthdate)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, first_name, second_name, phone_number, birthday, email, password=None):
+        if not first_name:
+            raise ValueError('A first name is required!')
+        if not second_name:
+            raise ValueError('A second name is required!')
+        if not email:
+            raise ValueError('An email is required!')
+        if not password:
+            raise ValueError('An password is required!')
+
+        user = self.create_user(first_name, second_name,
+                                phone_number, birthday, email, password)
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+    user_id = models.BigAutoField(primary_key=True)
+
+    email = models.CharField(max_length=254, unique=True, null=False)
+    phone_number = models.CharField(null=False, unique=True)
+
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+
+    birthday = models.DateField(null=False)
+
+    image = models.ImageField(upload_to="uploads/images/users/", null=True)
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateField(auto_now_add=True, null=False)
+
+    # Add related_name to avoid clashes with auth.Group's reverse accessor
+    groups = models.ManyToManyField(
+        'auth.Group', related_name='app_users', blank=True)
+    user_permissions = models.ManyToManyField(
+        'auth.Permission', related_name='app_users', blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'second_name']
+
+    objects = AppUserManager()
+
+    def __str__(self):
+        return self.username
 
 
 class MarketInstrument(models.Model):
@@ -39,7 +123,7 @@ class Stock(models.Model):
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(AppUser, on_delete=models.CASCADE)
     instrument = models.ForeignKey(MarketInstrument, on_delete=models.CASCADE)
     order_type = models.CharField(max_length=4, choices=order_type_choices)
     price = models.DecimalField(max_digits=10, decimal_places=2)
