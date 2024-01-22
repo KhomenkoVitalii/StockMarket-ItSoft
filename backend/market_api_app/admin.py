192b89ec862d798ce2a26720b1a4b0372093fd32
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2024-01-22 02:24:54 +0200

    feat admin: add models to the admin panel

--- backend/market_api_app/admin.py
+++ backend/market_api_app/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import AppUser
+from market_api_app import models
 
 # Register your models here.
-admin.site.register(AppUser)
+admin.site.register(models.AppUser)
+admin.site.register(models.MarketInstrument)
+admin.site.register(models.Industry)
+admin.site.register(models.Stock)
+admin.site.register(models.Order)
+admin.site.register(models.Transaction)
+admin.site.register(models.MarketData)
