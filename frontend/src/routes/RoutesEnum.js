e1fcde239fdd5cce17a5000334fcd1a2d27aad9b
KhomenkoVitalii <khomenko.vitalii02@gmail.com>
2024-01-31 01:37:12 +0200

    feat: create RoutesEnum.js

---
+++ frontend/src/routes/RoutesEnum.js
@@ -0,0 +1,9 @@
+const RoutesEnum = Object.freeze({
+    HOME: '/',
+    LOGIN: '/auth/login/',
+    REGISTER: '/auth/register/',
+    LOGOUT: '/auth/logout/',
+    MARKET: '/market/',
+});
+
+export default RoutesEnum;
